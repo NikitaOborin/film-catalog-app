54a9c8fe66bffc01fbecb845db1d54dc013ab41e
Oborin Nikita <oborinikita@yandex.ru>
2023-09-13 21:13:56 +0700

    Inital commit

---
+++ src/main/java/ru/yandex/filmcatalog/app/FilmCatalogApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.filmcatalog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmCatalogApplication.class, args);
+	}
+
+}
